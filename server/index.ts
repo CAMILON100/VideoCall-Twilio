61abc1b74fbb2d3658633bbecccac095e8df28a8
Omar Arenas <omararenasuni@gmail.com>
2022-03-25 12:16:34 -0500

    Start: commit antes de empezar

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,18 @@ import { createExpressHandler } from './createExpressHandler';
 import express, { RequestHandler } from 'express';
 import path from 'path';
 import { ServerlessFunction } from './types';
+import fs from 'fs';
+
+/* const options = {
+  key: fs.readFileSync("../../../../../../etc/letsencrypt/live/fitafter50.ml/privkey.pem"),
+  cert: fs.readFileSync("../../../../../../etc/letsencrypt/live/fitafter50.ml/cert.pem")
+}; */
+
 
 const PORT = process.env.PORT ?? 8081;
 
 const app = express();
+const server = require('http').createServer(app);
 app.use(express.json());
 
 // This server reuses the serverless endpoints from the "plugin-rtc" Twilio CLI Plugin, which is used when the "npm run deploy:twilio-cli" command is run.
@@ -14,16 +22,11 @@ app.use(express.json());
 const tokenFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
 const tokenEndpoint = createExpressHandler(tokenFunction);
 
-const recordingRulesFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/recordingrules')
-  .handler;
-const recordingRulesEndpoint = createExpressHandler(recordingRulesFunction);
-
 const noopMiddleware: RequestHandler = (_, __, next) => next();
 const authMiddleware =
   process.env.REACT_APP_SET_AUTH === 'firebase' ? require('./firebaseAuthMiddleware') : noopMiddleware;
 
 app.all('/token', authMiddleware, tokenEndpoint);
-app.all('/recordingrules', authMiddleware, recordingRulesEndpoint);
 
 app.use((req, res, next) => {
   // Here we add Cache-Control headers in accordance with the create-react-app best practices.
@@ -43,4 +46,4 @@ app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.listen(PORT, () => console.log(`twilio-video-app-react server running on ${PORT}`));
+server.listen(PORT, () => console.log(`twilio-video-app-react server running on ${PORT}`));
